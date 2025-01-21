1072b38414ba1c1d06807f4da7a32b6dacf32a5c
ImJaeOne <dlawi0108@naver.com>
2025-01-21 16:51:26 +0900

    ✨ update: MedalForm 추가 기능 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import MedalForm from './components/MedalForm'
-import './App.css'
+import { useState } from 'react';
+import MedalForm from './components/MedalForm';
+import './App.css';
 
 function App() {
+    const [medalList, setMedalList] = useState([]);
 
-  return (
-    <>
-      <MedalForm/>
-    </>
-  )
+    const handleSubmit = (e, value) => {
+        e.preventDefault();
+        setMedalList([...medalList, value]);
+    };
+    return (
+        <>
+            <MedalForm handleSubmit={handleSubmit} medalList={medalList} />
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/MedalForm.jsx
+++ src/components/MedalForm.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { React, useState } from 'react';
 
-const MedalForm = () => {
-    return <div>MedalForm</div>;
+const MedalForm = ({ handleSubmit }) => {
+    const [medalItem, setMedalItem] = useState({
+        country: '',
+        gold: 0,
+        silver: 0,
+        bronze: 0,
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setMedalItem({...medalItem, [name]: value });
+    };
+
+    return (
+        <div>
+            <h2>2024 파리 올림픽</h2>
+            <form onSubmit={(e) => handleSubmit(e, medalItem)}>
+                국가명
+                <input type="text" name="country" value={medalItem.country} onChange={handleChange} />
+                금메달
+                <input type="number" name="gold" value={medalItem.gold} onChange={handleChange} />
+                은메달
+                <input type="number" name="silver" value={medalItem.silver} onChange={handleChange} />
+                동메달
+                <input type="number" name="bronze" value={medalItem.bronze} onChange={handleChange} />
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    );
 };
 
 export default MedalForm;
