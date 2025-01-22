3c052f5b5e2a2c88a7365bf6b700b4f4f790a97f
ImJaeOne <dlawi0108@naver.com>
2025-01-22 10:50:05 +0900

    ✨ update: 메달 리스트 관리 로직을 useMedalList 커스텀 훅으로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,25 @@
-import { useState } from 'react';
+import { useMedalList } from './hooks/useMedalList.js';
 import MedalForm from './components/MedalForm';
 import MedalList from './components/MedalList';
-import { setStorage, getStorage, updateStorage, deleteStorageItem } from './sevices/storage';
 import './App.css';
 
 function App() {
-    const [medalList, setMedalList] = useState(getStorage('medalListStorage'));
+    const { medalList, addMedal, updateMedal, deleteMedal } = useMedalList('medalListStorage');
+
     const handleSubmit = (e, value, action) => {
         e.preventDefault();
         if (action === 'add') {
-            if (medalList.some((m) => m.country === value.country)) {
-                alert('이미 등록되어있습니다. 업데이트해주세요.');
-                return;
-            }
-            const addMedalList = [...medalList, value];
-            setMedalList(addMedalList);
-            setStorage('medalListStorage', addMedalList);
+            addMedal(value);
         } else if (action === 'update') {
-            if (!medalList.some((m) => m.country === value.country)) {
-                alert('등록된 나라가 없습니다. 등록해주세요');
-                return;
-            }
-            const updateMedalList = medalList.map((m) => (m.country === value.country ? { ...value } : m));
-            setMedalList(updateMedalList);
-            updateStorage('medalListStorage', updateMedalList);
+            updateMedal(value);
         }
     };
 
-    const handleDeleteList = (country) => {
-        const deletedList = medalList.filter((m) => {
-            return m.country !== country;
-        });
-        setMedalList(deletedList);
-        deleteStorageItem('medalListStorage', country);
-    };
     return (
         <>
             <h2>2024 파리 올림픽</h2>
             <MedalForm handleSubmit={handleSubmit} />
-            <MedalList handleDeleteList={handleDeleteList} medalList={medalList} />
+            <MedalList handleDeleteList={deleteMedal} medalList={medalList} />
         </>
     );
 }
