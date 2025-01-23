80b9f34f273b3d897fe5553bd9a823348cec363d
ImJaeOne <dlawi0108@naver.com>
2025-01-23 20:36:47 +0900

    ♻️ refactor: custom 훅 유효성 검사 분리

--- src/hooks/useMedalList.js
+++ src/hooks/useMedalList.js
@@ -1,32 +1,29 @@
 import { useState } from 'react';
 import { setStorage, getStorage, deleteStorageItem } from '../services/storage';
 
+const validateMedalForm = (value, medalList) => {
+    if (value.country === '' || value.gold === '' || value.silver === '' || value.bronze === '') {
+        alert('모든 값을 기입해주세요.');
+        return;
+    }
+    if (medalList.some((m) => m.country === value.country)) {
+        alert('이미 등록되어있습니다. 업데이트해주세요.');
+        return;
+    }
+}
+
 export const useMedalList = (key) => {
     const [medalList, setMedalList] = useState(getStorage(key) || []);
 
     const addMedal = (value) => {
-        if (value.country === '' || value.gold === '' || value.silver === '' || value.bronze === '') {
-            alert('모든 값을 기입해주세요.');
-            return;
-        }
-        if (medalList.some((m) => m.country === value.country)) {
-            alert('이미 등록되어있습니다. 업데이트해주세요.');
-            return;
-        }
+        validateMedalForm(value, medalList);
         const updatedList = [...medalList, value];
         setMedalList(updatedList);
         setStorage(key, updatedList);
     };
 
     const updateMedal = (value) => {
-        if (value.country === '' || value.gold === '' || value.silver === '' || value.bronze === '') {
-            alert('모든 값을 기입해주세요.');
-            return;
-        }
-        if (!medalList.some((m) => m.country === value.country)) {
-            alert('등록된 나라가 없습니다. 등록해주세요');
-            return;
-        }
+        validateMedalForm(value, medalList);
         const updatedList = medalList.map((m) => (m.country === value.country ? { ...value } : m));
         setMedalList(updatedList);
         setStorage(key, updatedList);
