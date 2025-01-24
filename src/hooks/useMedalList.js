7ea1ba770329aae54d47f31d8973556c4dde1813
ImJaeOne <dlawi0108@naver.com>
2025-01-24 15:12:21 +0900

    ✨ useMedalList 훅 유효성 검사 수정 및 deleteStorage 로직 삭제

--- src/hooks/useMedalList.js
+++ src/hooks/useMedalList.js
@@ -1,30 +1,39 @@
 import { useState } from 'react';
-import { setStorage, getStorage, deleteStorageItem } from '../services/storage';
+import { setStorage, getStorage } from '../services/storage';
 
-const validateMedalForm = (value, medalList) => {
+const validateMedalForm = (action, value, medalList) => {
     if (value.country === '' || value.gold === '' || value.silver === '' || value.bronze === '') {
         alert('모든 값을 기입해주세요.');
-        return;
+        return false;
     }
-    if (medalList.some((m) => m.country === value.country)) {
-        alert('이미 등록되어있습니다. 업데이트해주세요.');
-        return;
+    if (action === 'add') {
+        if (medalList.some((m) => m.country === value.country)) {
+            alert('이미 등록되어있습니다. 업데이트해주세요.');
+            return false;
+        }
     }
-}
+    if (action === 'update') {
+        if (!medalList.some((m) => m.country === value.country)) {
+            alert('추가된 국가가 없습니다. 국가를 추가해주세요.');
+            return false;
+        }
+    }
+    return true;
+};
 
 export const useMedalList = (key) => {
     const [medalList, setMedalList] = useState(() => {
-        return getStorage(key) || []
+        return getStorage(key) || [];
     });
     const addMedal = (value) => {
-        validateMedalForm(value, medalList);
+        if (!validateMedalForm('add', value, medalList)) return;
         const updatedList = [...medalList, value];
         setMedalList(updatedList);
         setStorage(key, updatedList);
     };
 
     const updateMedal = (value) => {
-        validateMedalForm(value, medalList);
+        if (!validateMedalForm('update', value, medalList)) return;
         const updatedList = medalList.map((m) => (m.country === value.country ? { ...value } : m));
         setMedalList(updatedList);
         setStorage(key, updatedList);
@@ -33,7 +42,7 @@ export const useMedalList = (key) => {
     const deleteMedal = (country) => {
         const updatedList = medalList.filter((m) => m.country !== country);
         setMedalList(updatedList);
-        deleteStorageItem(key, country);
+        setStorage(key, updatedList);
     };
 
     return { medalList, addMedal, updateMedal, deleteMedal };
--- src/services/storage.js
+++ src/services/storage.js
@@ -8,20 +8,11 @@ export const setStorage = (key, value) => {
 
 export const getStorage = (key) => {
     try {
+        console.log('getStorage 실행!');
         const value = localStorage.getItem(key);
         return value ? JSON.parse(value) : null;
     } catch (err) {
         console.error('Failed getStorage : ', err);
         return null;
-    }
-};
-
-export const deleteStorageItem = (key, country) => {
-    try {
-        const getStorageList = getStorage(key) || [];
-        const deleteListItem = getStorageList.filter((item) => item.country !== country);
-        setStorage(key, deleteListItem);
-    } catch (err) {
-        console.error('Failed to deleteStorageItem : ', err);
     }
 };
